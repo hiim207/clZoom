0aee88efdb2606cccd005516e635b0483ea2208d
hiim207 <20jin_lee@naver.com>
2023-11-28 10:52:14 +0900

    test1

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-console.log("hello Judith!");
+import express from 'express';
+
+const app = express();
+
+const handleListen = () => console.log("듣고 있어 http://localhost:3000");
+app.listen(3000, handleListen);
+
+
+
